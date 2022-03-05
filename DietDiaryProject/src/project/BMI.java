91d9a306c0442c469a094a4c9155a92634d56bd9
Wyna <98083311+Wyna2@users.noreply.github.com>
2022-03-05 21:52:12 +0900

    commit test1

---
+++ DietDiaryProject/src/project/BMI.java
@@ -0,0 +1,9 @@
+package project;
+
+public class BMI {
+
+	public static void main(String[] args) {
+		System.out.println("commit test");
+	}
+
+}
---
+++ DietDiaryProject/src/project/Diet.java
@@ -0,0 +1,9 @@
+package project;
+
+public class Diet {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
